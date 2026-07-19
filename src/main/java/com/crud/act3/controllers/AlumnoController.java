bea08a80a0b0e9afdf14c8f1746b6019a2d475a8
4lE10x0 <ale10mu5chz@gmail.com>
2026-07-19 01:01:44 -0600

    modificacion a sliders

--- src/main/java/com/crud/act3/controllers/AlumnoController.java
+++ src/main/java/com/crud/act3/controllers/AlumnoController.java
@@ -9,7 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
-@RequestMapping("/alumnos")
+@RequestMapping("/")
 public class AlumnoController {
 
     @Autowired
